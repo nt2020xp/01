9e5ccfb51724e50d92f28af07aac110567055380
nt2020xp <nt01xp@gmail.com>
2025-10-13 20:29:18 +0800

    Create tv.js

---
+++ tv.js
@@ -0,0 +1,154 @@
+var tvdata = [
+
+
+	
+		 {
+		
+        groupid: 1,
+        groupname: "中央频道",
+        tvlist: [
+            { id: 1, logo: "https://p1.p5dxkjm8.com/tv/img/1.png", title: "CCTV-1", des: "综合", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/8ispgQnNg/8ispgQnNg_live.m3u8" },
+            { id: 2, logo: "https://p1.p5dxkjm8.com/tv/img/2.png", title: "CCTV-2", des: "财经", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/K_CpRQ7HR/K_CpRQ7HR_live.m3u8" },
+            { id: 3, logo: "https://p1.p5dxkjm8.com/tv/img/3.png", title: "CCTV-3", des: "综艺", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/cSGhRw7Hg/cSGhRw7Hg_live.m3u8" },
+            { id: 4, logo: "https://p1.p5dxkjm8.com/tv/img/4.png", title: "CCTV-4", des: "国际", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/8rd2RQ7Ng/8rd2RQ7Ng_live.m3u8" },
+            { id: 6, logo: "https://p1.p5dxkjm8.com/tv/img/6.png", title: "CCTV-6", des: "电影", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/v7q2gw7HR/v7q2gw7HR_live.m3u8" },
+            { id: 7, logo: "https://p1.p5dxkjm8.com/tv/img/7.png", title: "CCTV-7", des: "国防军事", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/h2VoRQ7HR/h2VoRQ7HR_live.m3u8" },
+            { id: 8, logo: "https://p1.p5dxkjm8.com/tv/img/8.png", title: "CCTV-8", des: "电视剧", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/VScTgwnHR/VScTgwnHR_live.m3u8" },
+            { id: 9, logo: "https://p1.p5dxkjm8.com/tv/img/9.png", title: "CCTV-9", des: "记录", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/NzboRw7Hg/NzboRw7Hg_live.m3u8" },
+            { id: 10, logo: "https://p1.p5dxkjm8.com/tv/img/10.png", title: "CCTV-10", des: "科教", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/yx8oRw7NR/yx8oRw7NR_live.m3u8" },
+            { id: 13, logo: "https://p1.p5dxkjm8.com/tv/img/13.png", title: "CCTV-13", des: "新闻", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/oMBDMw7HR/oMBDMw7HR_live.m3u8" },
+
+
+
+
+
+
+
+
+
+        ]
+    },
+
+
+
+    {
+        groupid: 2,
+        groupname: "地方卫视",
+        tvlist: [
+            { id: 18, logo: "https://p1.p5dxkjm8.com/tv/img/北京.png", title: "北京卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/EeE1Rw7Hgz/EeE1Rw7Hgz_live.m3u8" },
+            { id: 19, logo: "https://p1.p5dxkjm8.com/tv/img/安徽.png", title: "安徽卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/ZbU6ZQnNg/ZbU6ZQnNg_live.m3u8" },
+            { id: 20, logo: "https://p1.p5dxkjm8.com/tv/img/东南.png", title: "东南卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/7J7MUM3SR/7J7MUM3SR_live.m3u8" },
+            { id: 21, logo: "https://p1.p5dxkjm8.com/tv/img/甘肃.png", title: "甘肃卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/TMDM8M3Sg/TMDM8M3Sg_live.m3u8" },
+            { id: 22, logo: "https://p1.p5dxkjm8.com/tv/img/广东.png", title: "广东卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/Y5tMUMqSRz/Y5tMUMqSRz_live.m3u8" },
+            { id: 23, logo: "https://p1.p5dxkjm8.com/tv/img/广西1.png", title: "广西卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/XA1a8MqSR/XA1a8MqSR_live.m3u8" },
+            { id: 24, logo: "https://p1.p5dxkjm8.com/tv/img/贵州.png", title: "贵州卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/r4PM8G3SRz/r4PM8G3SRz_live.m3u8" },
+            { id: 25, logo: "https://p1.p5dxkjm8.com/tv/img/海南.png", title: "海南卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/xZnB8M3Ig/xZnB8M3Ig_live.m3u8" },
+            { id: 26, logo: "https://p1.p5dxkjm8.com/tv/img/河北.png", title: "河北卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/ppDB8GqIR/ppDB8GqIR_live.m3u8" },
+            { id: 27, logo: "https://p1.p5dxkjm8.com/tv/img/河南.png", title: "河南卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/mYtfUGqSR/mYtfUGqSR_live.m3u8" },
+            { id: 28, logo: "https://p1.p5dxkjm8.com/tv/img/黑龙江.png", title: "黑龙江卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/43Jf8GqIg/43Jf8GqIg_live.m3u8" },
+            { id: 29, logo: "https://p1.p5dxkjm8.com/tv/img/湖北.png", title: "湖北卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/7pPf8MqSR/7pPf8MqSR_live.m3u8" },
+            { id: 30, logo: "https://p1.p5dxkjm8.com/tv/img/湖南.png", title: "湖南卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/g9lfUG3SR/g9lfUG3SR_live.m3u8" },
+            { id: 31, logo: "https://p1.p5dxkjm8.com/tv/img/吉林.png", title: "吉林卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/_ZefUG3Ig/_ZefUG3Ig_live.m3u8" },
+            { id: 32, logo: "https://p1.p5dxkjm8.com/tv/img/江苏.png", title: "江苏卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/jbZYUGqSg/jbZYUGqSg_live.m3u8" },
+            { id: 33, logo: "https://p1.p5dxkjm8.com/tv/img/江西.png", title: "江西卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/_LSL8MqIR/_LSL8MqIR_live.m3u8" },
+            { id: 34, logo: "https://p1.p5dxkjm8.com/tv/img/辽宁.png", title: "辽宁卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldn/GjKY8MqIR/GjKY8MqIR_live.m3u8" },
+
+            { id: 35, logo: "https://p1.p5dxkjm8.com/tv/img/青海.png", title: "青海卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlnt/ub_gwG3Ig/ub_gwG3Ig_live.m3u8" },
+            { id: 36, logo: "https://p1.p5dxkjm8.com/tv/img/山东.png", title: "山东卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlnt/BCeRwG3Sg/BCeRwG3Sg_live.m3u8" },
+            { id: 37, logo: "https://p1.p5dxkjm8.com/tv/img/深圳.png", title: "深圳卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlnt/_e7zQMqSRz/_e7zQMqSRz_live.m3u8" },
+            { id: 38, logo: "https://p1.p5dxkjm8.com/tv/img/四川.png", title: "四川卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlnt/h3FzQG3IRz/h3FzQG3IRz_live.m3u8" },
+            { id: 39, logo: "https://p1.p5dxkjm8.com/tv/img/天津.png", title: "天津卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlnt/5f1zQM3SRz/5f1zQM3SRz_live.m3u8" },
+            { id: 40, logo: "https://p1.p5dxkjm8.com/tv/img/云南.png", title: "云南卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlnt/mDPzQMqIg/mDPzQMqIg_live.m3u8" },
+            { id: 41, logo: "https://p1.p5dxkjm8.com/tv/img/浙江.png", title: "浙江卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlnt/r9_zwMqSgz/r9_zwMqSgz_live.m3u8" },
+            { id: 42, logo: "https://p1.p5dxkjm8.com/tv/img/重庆.png", title: "重庆卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlnt/Et6zQMqSR/Et6zQMqSR_live.m3u8" },
+            { id: 43, logo: "https://p1.p5dxkjm8.com/tv/img/东方.png", title: "上海东方卫视", des: "电视台直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlnt/bJWiwGqIgz/bJWiwGqIgz_live.m3u8" },
+
+
+
+        ]
+    },
+
+
+
+    {
+        groupid: 3,
+        groupname: "影视频道",
+        tvlist: [
+		
+		     { id: 231, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "周星驰电影", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldb/sXSaSJBHg/sXSaSJBHg_live.m3u8" },
+
+           { id: 233, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "林正英电影", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldb/hN5MS1fHg/hN5MS1fHg_live.m3u8" },
+
+           { id: 235, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "王晶港风电影", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldb/siEaSJfHg/siEaSJfHg_live.m3u8" },
+
+
+           { id: 131, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "科幻电影①", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/GQ4M_9xNg/GQ4M_9xNg_live.m3u8" },
+
+           { id: 132, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "科幻电影②", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/oaQMl9xNR/oaQMl9xNR_live.m3u8" },
+
+           { id: 133, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "科幻电影③", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/xw3B_rbNg/xw3B_rbNg_live.m3u8" },
+
+          { id: 134, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "港片电影①", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/dXdL_rxHR/dXdL_rxHR_live.m3u8" },
+
+           { id: 135, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "港片电影②", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/ZGEY_rbNg/ZGEY_rbNg_live.m3u8" },
+
+            { id: 136, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "港片电影③", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/oaQMl9xNR/oaQMl9xNR_live.m3u8" },
+
+           { id: 138, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "超级英雄", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/P_8s_rbNg/P_8s_rbNg_live.m3u8" },
+
+           { id: 139, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "末日系列", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/bOZQ_9xNg/bOZQ_9xNg_live.m3u8" },
+
+          { id: 140, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "漫威电影", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/NuMs_rxHg/NuMs_rxHg_live.m3u8" },
+
+           { id: 141, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "精选动作", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/ZAQAl9bNg/ZAQAl9bNg_live.m3u8" },
+
+          { id: 142, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "惊悚电影", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/SLDwlrxNR/SLDwlrxNR_live.m3u8" },
+
+           { id: 143, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "国外动作", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/Y0C1_rbNg/Y0C1_rbNg_live.m3u8" },
+
+           { id: 144, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "国内喜剧", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/gf0U_9xHR/gf0U_9xHR_live.m3u8" },
+
+           { id: 145, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "国内动作", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlca/MboJlrxNR/MboJlrxNR_live.m3u8" },
+
+
+
+
+
+        ]
+    },
+
+
+
+    {
+        groupid: 4,
+        groupname: "综艺小品",
+        tvlist: [
+	
+            { id: 57, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "沈腾马丽经典小品", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldb/Qk7QAIqIR/Qk7QAIqIR_live.m3u8" },
+            { id: 53, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "宋小宝经典小品", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldb/ekC_QG3IR/ekC_QG3IR_live.m3u8" },
+            { id: 54, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "贾玲经典小品", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldb/zKzXQMqSR/zKzXQMqSR_live.m3u8" },
+            { id: 55, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "经典小品下饭必备", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldb/yl7uQGqSR/yl7uQGqSR_live.m3u8" },
+            { id: 56, logo: "https://p1.p5dxkjm8.com/tv/douyu.jpg", title: "全体起立你的本山大叔", des: "斗鱼直播", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hldb/CydXwM3IR/CydXwM3IR_live.m3u8" },
+
+
+
+        ]
+    },
+
+    {
+        groupid: 5,
+        groupname: "体育频道",
+        tvlist: [		
+		
+            { id: 5, logo: "https://p1.p5dxkjm8.com/tv/img/5.png", title: "CCTV-5", des: "体育", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/svAhgwnHR/svAhgwnHR_live.m3u8" },
+            { id: 17, logo: "https://p1.p5dxkjm8.com/tv/img/5p.png", title: "CCTV-5+", des: "体育", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/eYPhRQnNg/eYPhRQnNg_live.m3u8" },
+
+		    { id: 16, logo: "https://p1.p5dxkjm8.com/tv/img/16.png", title: "CCTV-16", des: "奥林匹克", coverimg: "https://p1.p5dxkjm8.com/tv/tv.jpg",  vurl: "https://6663.luyazo.com/livetv/hlcc/McNJgw7Hg/McNJgw7Hg_live.m3u8" },
+
+
+        ]
+    },
+
+
+];
+
